c0c522071f4ac00501cd0c53a47049bce297f1c5
natanri <riosjhonatan44@gmail.com>
2022-01-29 21:48:03 -0500

    Added my first line in team.

--- src/thomasson/Main.java
+++ src/thomasson/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jared Thomasson");
         System.out.println("Hello Jared, my name is Marcelo");
+        System.out.println("Hello Jared, my name is Jonathan");
     }
 }
