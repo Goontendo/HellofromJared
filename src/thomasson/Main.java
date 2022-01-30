287c7dbefedb4f5d18afe83d445e2ad60d72d13b
Marcelo <cas21014@byui.edu>
2022-01-29 22:58:57 -0300

    Added a fellow greeting to Jared.

--- src/thomasson/Main.java
+++ src/thomasson/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jared Thomasson");
+        System.out.println("Hello Jared, my name is Marcelo");
     }
+
 }
